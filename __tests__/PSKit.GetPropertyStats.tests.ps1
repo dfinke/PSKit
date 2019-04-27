31258b8c1bc894a2098740f4664abb456375f6d8
dfinke <finked@hotmail.com>
2019-04-27 09:36:26 -0400

    Refactor test

--- __tests__/PSKit.GetPropertyStats.tests.ps1
+++ __tests__/PSKit.GetPropertyStats.tests.ps1
@@ -3,21 +3,21 @@ Import-Module $PSScriptRoot\..\PSKit.psd1 -Force
 Describe "PSKit tests - Get-PropertyStat-InputObject s" {
     BeforeAll {
         $script:data = ConvertFrom-Csv @"
-Name,Class,Dorm,Room,GPA
-Sally Whittaker,2018,McCarren House,312,3.75
-Belinda Jameson,2017,Cushing House,148,3.52
-Jeff Smith,2018,Prescott House,17-D,3.2
-Sandy Allen,2019,Oliver House,108,3.48
-"@
-    }
-
-    It "Should calculate property stats when piped" {
-        $data = ConvertFrom-Csv @"
 Name,Age
 Jane,10
 John,5
 ,15
 "@
+        <#
+            ColumnName DataType HasNulls Min Max Avg Sum
+            ---------- -------- -------- --- --- --- ---
+            Name       string      False
+            Age        int         False 5   15  10  30
+        #>
+
+    }
+
+    It "Should calculate property stats when piped" {
         $actual = $data | Get-PropertyStats
 
         $actual.Count | Should Be 2
@@ -40,19 +40,6 @@ John,5
         $actual[1].Sum | Should Be 30    }
 
     It "Should calculate property stats" {
-        $data = ConvertFrom-Csv @"
-Name,Age
-Jane,10
-John,5
-,15
-"@
-
-        <#
-            ColumnName DataType HasNulls Min Max Avg Sum
-            ---------- -------- -------- --- --- --- ---
-            Name       string      False
-            Age        int         False 5   15  10  30
-        #>
 
         $actual = Get-PropertyStats -InputObject $data
         $actual.Count | Should Be 2
