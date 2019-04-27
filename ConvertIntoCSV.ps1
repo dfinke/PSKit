42a7949499a496d9e3f5c468d5fcd3fc38fcc97f
dfinke <finked@hotmail.com>
2019-04-27 09:25:19 -0400

    Updated inline help

--- ConvertIntoCSV.ps1
+++ ConvertIntoCSV.ps1
@@ -1,10 +1,23 @@
-# Needs test-path
-# Needs requires for ImportExcel
+<#
+    .SYNOPSIS
+    Converts files to CSV, from either JSON or Excel
+
+    .DESCRIPTION
+    Target either a JSON or Excel file and it will be converted to the CSV format. Note: The Excel file works only if there is a sheet named `Sheet1`
+
+    .EXAMPLE
+    Convert-IntoCSV C:\Temp\testData.xlsx
+
+    .EXAMPLE
+    Convert-IntoCSV C:\Temp\testData.json
+#>
 function Convert-IntoCSV {
     param(
         $Path
     )
 
+    # Needs test-path
+    # Needs requires for ImportExcel
     $ext = [System.IO.Path]::GetExtension($path)
 
     switch ($ext) {
