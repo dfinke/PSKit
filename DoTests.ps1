98f49aef896db94118186931c1806e1ffdc97ea9
dfinke <finked@hotmail.com>
2019-05-14 10:51:53 -0400

    update

--- DoTests.ps1
+++ DoTests.ps1
@@ -1,8 +1,16 @@
 $PSVersionTable.PSVersion
 
-if ($null -eq (Get-Module -ListAvailable pester)) {
-    Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
+$psModules = 'Pester', 'ImportExcel', 'PSStringScanner'
+
+foreach ($module in $psModules) {
+    if ($null -eq (Get-Module -ListAvailable $module)) {
+        Install-Module -Name $module -Repository PSGallery -Force -Scope CurrentUser
+    }
 }
+
+# if ($null -eq (Get-Module -ListAvailable pester)) {
+#     Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
+# }
 
 $result = Invoke-Pester -Script $PSScriptRoot\__tests__ -Verbose -PassThru
 
