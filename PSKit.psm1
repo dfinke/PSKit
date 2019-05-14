5e780da67aa0924aa02baf5db13f111e715310bf
dfinke <finked@hotmail.com>
2019-05-14 10:28:16 -0400

    fix casing for linux

--- PSKit.psm1
+++ PSKit.psm1
@@ -1,5 +1,5 @@
 ﻿. $PSScriptRoot/GetPropertyName.ps1
 . $PSScriptRoot/GetPropertyStats.ps1
-. $PSScriptRoot/inferData.ps1
+. $PSScriptRoot/InferData.ps1
 . $PSScriptRoot/NewLookupTable.ps1
 . $PSScriptRoot/ConvertIntoCSV.ps1
