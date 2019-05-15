c1144347014207cf9794308f74fa1b69c88b4ba1
dfinke <finked@hotmail.com>
2019-05-15 17:58:09 -0400

    tests, transpile and code gen

--- InvokeTranspileSQL.ps1
+++ InvokeTranspileSQL.ps1
@@ -1,10 +1,131 @@
 #requires -Modules PSStringScanner
 
+function ConvertFrom-SQLToPS {
+    param(
+        [Parameter(Mandatory)]
+        $SQL
+    )
+
+    Invoke-TranspileSQL $SQL | ConvertFrom-TranspileSQL
+}
+
+function Add-PSOp {
+    param($target)
+
+    $target | Add-Member -PassThru -MemberType ScriptProperty -Name PSOp -Value {
+        switch ($this.operation) {
+            "<>" { "-ne" }
+            ">=" { "-ge" }
+            "<=" { "-le" }
+            "=" { "-eq" }
+            ">" { "-gt" }
+            "<" { "-lt" }
+            default { $_ }
+        }
+    }
+}
+
+function Add-PSLogicOp {
+    param($target)
+
+    $target | Add-Member -PassThru -MemberType ScriptProperty -Name PSLogicOp -Value {
+        if ($this.LogicOp) {
+            "-" + $this.LogicOp
+        }
+    }
+}
+
 function Invoke-TranspileSQL {
     param(
         [Parameter(Mandatory)]
         $SQL
     )
 
-    $SQL
+    $ss = New-PSStringScanner $sql
+
+    $SELECT_KW = "^[Ss][Ee][Ll][Ee][Cc][Tt]\s+"
+    $FROM_KW = "[Ff][Rr][Oo][Mm]"
+    $WHERE_KW = "[Ww][Hh][Ee][Rr][Ee]"
+    $OPERATIONS = "<>|<=|>=|>|<|="
+    $LOGICAL = "[Oo][rR]|[Aa][Nn][Dd]"
+    $WHITESPACE = "\s+"
+
+    $h = [Ordered]@{ }
+
+    if ($ss.Check($SELECT_KW)) {
+        $null = $ss.Scan($SELECT_KW)
+
+        $h.SelectPropertyNames = ($ss.ScanUntil("(?=$FROM_KW)")).trim()
+
+        if ($h.SelectPropertyNames.Contains(',')) {
+            $h.SelectPropertyNames = $h.SelectPropertyNames.Split(',').foreach( { $_.trim() })
+        }
+
+        $null = $ss.Skip($FROM_KW)
+
+        if ($ss.Check($WHERE_KW)) {
+            $h.DataSetName = $ss.ScanUntil("(?=$WHERE_KW)").trim()
+            $null = $ss.Skip("$WHERE_KW")
+            $rest = $ss.Scan(".*")
+            $h.rest = $rest
+            $ssWhere = New-PSStringScanner $rest
+
+            $whereResults = @()
+
+            while (!$ssWhere.EoS()) {
+                $currentResult = [Ordered]@{ }
+                $currentResult.propertyName = $ssWhere.ScanUntil("(?=$OPERATIONS)").trim()
+                $currentResult.operation = $ssWhere.Scan($OPERATIONS)
+
+                if ($ssWhere.Check("$($WHITESPACE)$($LOGICAL)")) {
+                    $currentResult.value = $ssWhere.ScanUntil("(?=$($WHITESPACE)$($LOGICAL))")
+                    $currentResult.logicOp = $ssWhere.Scan($LOGICAL)
+                }
+                else {
+                    $currentResult.value = $ssWhere.Scan('.*').Trim()
+                }
+
+                $obj = Add-PSOp ([PSCustomObject]$currentResult)
+                $obj = Add-PSLogicOp $obj
+
+                $whereResults += [PSCustomObject]$obj
+            }
+        }
+        else {
+            $h.DataSetName = $ss.Scan(".*").trim()
+        }
+    }
+
+    if ($whereResults) {
+        $h.where = [PSCustomObject[]]$whereResults
+    }
+    $h
+}
+
+function ConvertFrom-TranspileSQL {
+    param(
+        [Parameter(ValueFromPipeline)]
+        [System.Collections.Specialized.OrderedDictionary]
+        $map
+    )
+
+    $SelectPropertyNames = $map.SelectPropertyNames
+
+    if ($SelectPropertyNames -ne '*') {
+        $SelectPropertyNames = $SelectPropertyNames -join '","'
+        $SelectPropertyNames = '"' + $SelectPropertyNames + '"'
+    }
+
+    if ($map.Contains("where")) {
+        $sqlResult += "| Where-Object {"
+        foreach ($whereRecord in $map.Where) {
+            $sqlResult += '$_.{0} {1} {2} {3} ' -f $whereRecord.propertyName, $whereRecord.PSOp, $whereRecord.value.trim(), $whereRecord.PSLogicOp
+            if ($null -eq $whereRecord.PSLogicOp) { $sqlResult = $sqlResult.Trim() }
+        }
+        $sqlResult += "}"
+    }
+
+    $sqlResult += " | Select-Object -Property $($SelectPropertyNames)"
+
+    $sqlResult
 }
--- __tests__/PSKit.InvokeTranspileSQL.tests.ps1
+++ __tests__/PSKit.InvokeTranspileSQL.tests.ps1
@@ -1,9 +1,115 @@
 Import-Module $PSScriptRoot/../PSKit.psd1 -Force
 Describe "PSKit tests - Invoke-TranspileSQL" {
-
-    It "Data should have 3 records" {
+    It "Should have a select and from value" {
         $actual = Invoke-TranspileSQL "Select * FROM X"
 
-        $actual | Should BeExactly "Select * FROM X"
+        @($actual.SelectPropertyNames).Count | Should Be 1
+        $actual.SelectPropertyNames | Should BeExactly '*'
+        $actual.DataSetName | Should BeExactly 'X'
     }
+
+    It "Should have 2 select values and a from value" {
+        $actual = Invoke-TranspileSQL "Select cash, name FROM X"
+
+        @($actual.SelectPropertyNames).Count | Should Be 2
+        $actual.SelectPropertyNames | Should BeExactly 'cash', 'name'
+        $actual.DataSetName | Should BeExactly 'X'
+    }
+
+    It "Should have 2 select values and a from value even with empty Where clause" {
+        $actual = Invoke-TranspileSQL "Select cash, name FROM X"
+
+        @($actual.SelectPropertyNames).Count | Should Be 2
+        $actual.SelectPropertyNames | Should BeExactly 'cash', 'name'
+        $actual.DataSetName | Should BeExactly 'X'
+    }
+
+    It "Should have 2 select values and a from value and a Where clause" {
+        $actual = Invoke-TranspileSQL "Select cash, name FROM X Where age >= 44 and age <= 50"
+
+        @($actual.SelectPropertyNames).Count | Should Be 2
+        $actual.SelectPropertyNames | Should BeExactly 'cash', 'name'
+        $actual.DataSetName | Should BeExactly 'X'
+
+        $actual.where.Count | Should Be 2
+
+        $actual.where[0].propertyName | Should BeExactly 'age'
+        $actual.where[0].operation | Should Be '>='
+        $actual.where[0].value | Should BeExactly 44
+        $actual.where[0].logicOp | Should BeExactly 'and'
+        $actual.where[0].PSOp | Should BeExactly '-ge'
+        $actual.where[0].PSLogicOp | Should BeExactly '-and'
+
+        $actual.where[1].propertyName | Should BeExactly 'age'
+        $actual.where[1].operation | Should Be '<='
+        $actual.where[1].value | Should BeExactly 50
+        $actual.where[1].logicOp | Should Be $null
+        $actual.where[1].PSOp | Should BeExactly '-le'
+        $actual.where[1].PSLogicOp | Should Be $null
+    }
+}
+
+Describe "PSKit tests - ConvertFrom-TranspileSQL" {
+    It "Should translate *" {
+        $actual = Invoke-TranspileSQL "Select * FROM X" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly ' | Select-Object -Property *'
+    }
+
+    It "Should translate a select value" {
+        $actual = Invoke-TranspileSQL "Select cash FROM X" | ConvertFrom-TranspileSQL
+
+        $actual | Should BeExactly ' | Select-Object -Property "cash"'
+    }
+
+    It "Should translate many select values" {
+        $actual = Invoke-TranspileSQL "Select cash, name FROM X" | ConvertFrom-TranspileSQL
+
+        $actual | Should BeExactly ' | Select-Object -Property "cash","name"'
+    }
+
+    It "Should translate select and where =" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age = 34" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -eq 34} | Select-Object -Property *'
+    }
+
+    It "Should translate select and where >" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age > 34" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -gt 34} | Select-Object -Property *'
+    }
+
+    It "Should translate select and where <" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age < 34" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -lt 34} | Select-Object -Property *'
+    }
+
+    It "Should translate select and where >=" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age >= 34" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -ge 34} | Select-Object -Property *'
+    }
+
+    It "Should translate select and where <=" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age <= 34" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -le 34} | Select-Object -Property *'
+    }
+
+    It "Should translate select and where <>" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age <> 34" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly "| Where-Object {`$_.age -ne 34} | Select-Object -Property *"
+    }
+
+    It "Should translate select and where <> 'abc" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age <> 'abc'" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly "| Where-Object {`$_.age -ne 'abc'} | Select-Object -Property *"
+    }
+
+    It "Should translate select and where > and <" {
+        $actual = Invoke-TranspileSQL "Select * FROM X where age > 34 and age < 70" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -gt 34 -and $_.age -lt 70} | Select-Object -Property *'
+    }
+
+    It "Should translate multi select and where > and <" {
+        $actual = Invoke-TranspileSQL "Select cash, name FROM X where age > 34 and age < 70" | ConvertFrom-TranspileSQL
+        $actual | Should BeExactly '| Where-Object {$_.age -gt 34 -and $_.age -lt 70} | Select-Object -Property "cash","name"'
+    }
+
 }
