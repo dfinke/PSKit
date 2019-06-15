844f70b46c3e408d831b856c9c63e71cb085b28f
dfinke <finked@hotmail.com>
2019-06-15 16:13:43 -0400

    Setup properties prior to stats calc

--- GenerateStats.ps1
+++ GenerateStats.ps1
@@ -23,16 +23,26 @@ function GenerateStats {
 
         $h.DataType = $DataType
         $h.HasNulls = if ($DataType) { @($TargetData.$name -match '^$').count -gt 0 }
+        $h.Min = $null
+        $h.Max = $null
+        $h.Median = $null
+        $h.StandardDeviation = $null
+        $h.Variance = $null
+        $h.Sum = $null
 
         $validDataTypes = 'int|double|float|decimal'
 
-        $h.Min = if ($DataType -match $validDataTypes) { [MathNet.Numerics.Statistics.Statistics]::Minimum([double[]]$TargetData.$name) }
-        $h.Max = if ($DataType -match $validDataTypes) { [MathNet.Numerics.Statistics.Statistics]::Maximum([double[]]$TargetData.$name) }
-        $h.Median = if ($DataType -match $validDataTypes) { [MathNet.Numerics.Statistics.Statistics]::Median([double[]]$TargetData.$name) }
-        $h.StandardDeviation = if ($DataType -match $validDataTypes) { [MathNet.Numerics.Statistics.Statistics]::StandardDeviation([double[]]$TargetData.$name) }
-        $h.Variance = if ($DataType -match $validDataTypes) { [MathNet.Numerics.Statistics.Statistics]::Variance([double[]]$TargetData.$name) }
-
-        $h.Sum = if ($DataType -match $validDataTypes) { ($TargetData.$name | Measure-Object -Sum).Sum }
+        $stats = [MathNet.Numerics.Statistics.Statistics]
+        switch -Regex ($DataType) {
+            $validDataTypes {
+                $h.Min = $stats::Minimum([double[]]$TargetData.$name)
+                $h.Max = $stats::Maximum([double[]]$TargetData.$name)
+                $h.Median = $stats::Median([double[]]$TargetData.$name)
+                $h.StandardDeviation = $stats::StandardDeviation([double[]]$TargetData.$name)
+                $h.Variance = $stats::Variance([double[]]$TargetData.$name)
+                $h.Sum = ($TargetData.$name | Measure-Object -Sum).Sum
+            }
+        }
 
         [PSCustomObject]$h
     }
