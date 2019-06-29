050c10f5772c14f06e3049f02dddbaad6c36f09d
Irwin Strachan <irwins@users.noreply.github.com>
2019-06-29 11:03:39 +0200

    fix: Rearranged Test PropertyStats

--- __tests__/PSKit.GetPropertyStats.tests.ps1
+++ __tests__/PSKit.GetPropertyStats.tests.ps1
@@ -11,53 +11,71 @@ John,5
         <#
             ColumnName DataType HasNulls Min Max Avg Sum
             ---------- -------- -------- --- --- --- ---
-            Name       string      False
+            Name       string      True
             Age        int         False 5   15  10  30
         #>
 
     }
 
-    It "Should calculate property stats when piped" {
+    Context "Should calculate property stats when piped [`$data | Get-PropertyStats]" {
         $actual = $data | Get-PropertyStats
 
-        $actual.Count | Should Be 2
+        It "Count Data sample should be [2]"{
+            $actual.Count | Should Be 2
+        }
 
-        $actual[0].ColumnName | Should BeExactly 'Name'
-        $actual[0].DataType | Should BeExactly 'string'
-        $actual[0].HasNulls | Should Be $true
+        It "Column [Name] is of DataType [String] and HasNulls [True]"{
+            $actual[0].ColumnName | Should BeExactly 'Name'
+            $actual[0].DataType   | Should BeExactly 'string'
+            $actual[0].HasNulls    | Should Be $true
 
-        $actual[0].Min | Should Be $null
-        $actual[0].Max | Should Be $null
-        $actual[0].Avg | Should Be $null
-        $actual[0].Sum | Should Be $null
+            $actual[0].Min | Should Be $null
+            $actual[0].Max | Should Be $null
+            $actual[0].Avg | Should Be $null
+            $actual[0].Sum | Should Be $null
+        }
 
-        $actual[1].ColumnName | Should BeExactly 'Age'
-        $actual[1].DataType | Should BeExactly 'int'
-        $actual[1].HasNulls | Should Be $false
-        $actual[1].Min | Should Be 5
-        $actual[1].Max | Should Be 15
-        $actual[1].Avg | Should Be 10
-        $actual[1].Sum | Should Be 30    }
+        It "Column [Age] is of DataType [Int] HasNulls [False]"{
+            $actual[1].ColumnName | Should BeExactly 'Age'
+            $actual[1].DataType   | Should BeExactly 'int'
+            $actual[1].HasNulls   | Should Be $false
+        }
 
-    It "Should calculate property stats" {
+        It "Min [5] Max [15] Avg [10] Sum [30]"{
+            $actual[1].Min | Should Be 5
+            $actual[1].Max | Should Be 15
+            $actual[1].Avg | Should Be 10
+            $actual[1].Sum | Should Be 30    
+        }
+    }
 
-        $actual = Get-PropertyStats -InputObject $data
-        $actual.Count | Should Be 2
+    Context "Should calculate property stats [Get-PropertyStats -InputObject `$data]" {
+        It "Count Data sample should be [2]"{
+            $actual = Get-PropertyStats -InputObject $data
+            $actual.Count | Should Be 2
+        }
 
-        $actual[0].ColumnName | Should BeExactly 'Name'
-        $actual[0].DataType | Should BeExactly 'string'
-        $actual[0].HasNulls | Should Be $true
-        $actual[0].Min | Should Be $null
-        $actual[0].Max | Should Be $null
-        $actual[0].Avg | Should Be $null
-        $actual[0].Sum | Should Be $null
+        It "Column [Name] is of DataType [String] and HasNulls [True]"{
+            $actual[0].ColumnName | Should BeExactly 'Name'
+            $actual[0].DataType | Should BeExactly 'string'
+            $actual[0].HasNulls | Should Be $true
+            $actual[0].Min | Should Be $null
+            $actual[0].Max | Should Be $null
+            $actual[0].Avg | Should Be $null
+            $actual[0].Sum | Should Be $null
+        }
 
-        $actual[1].ColumnName | Should BeExactly 'Age'
-        $actual[1].DataType | Should BeExactly 'int'
-        $actual[1].HasNulls | Should Be $false
-        $actual[1].Min | Should Be 5
-        $actual[1].Max | Should Be 15
-        $actual[1].Avg | Should Be 10
-        $actual[1].Sum | Should Be 30
+        It "Column [Age] is of DataType [Int] HasNulls [False]"{
+            $actual[1].ColumnName | Should BeExactly 'Age'
+            $actual[1].DataType | Should BeExactly 'int'
+            $actual[1].HasNulls | Should Be $false
+        }
+
+        It "Min [5] Max [15] Avg [10] Sum [30]"{
+            $actual[1].Min | Should Be 5
+            $actual[1].Max | Should Be 15
+            $actual[1].Avg | Should Be 10
+            $actual[1].Sum | Should Be 30
+        }
     }
 }
