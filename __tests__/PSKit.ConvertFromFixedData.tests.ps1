ed72399b1ce4fe5533e50689b60216044bd74b3d
Irwin Strachan <irwins@users.noreply.github.com>
2019-06-29 10:17:36 +0200

    fix: Added test verify Trim

--- __tests__/PSKit.ConvertFromFixedData.tests.ps1
+++ __tests__/PSKit.ConvertFromFixedData.tests.ps1
@@ -19,13 +19,23 @@ Joe  34 83
 "@ -split "\r?\n"
     }
 
-    It "Data should convert from fixed Data" {
+    Context "Data should convert from fixed Data" {
         $actual = ConvertFrom-FixedData $script:fixedData $script:schema
 
-        $actual.Count | Should Be 4
+        It "Sampled Data count should be [4]"{
+            $actual.Count | Should Be 4
+        }
 
-        $actual[1].name | Should BeExactly 'Brian'
-        $actual[1].age | Should Be 26
-        $actual[1].cash | Should Be 110
+        It "Should convert fixed Data [Brian26110] to ['Brian','26','110']"{
+            $actual[1].name | Should BeExactly 'Brian'
+            $actual[1].age  | Should Be 26
+            $actual[1].cash | Should Be 110
+        }
+
+        It "Whitespaces should be trimmed from Data [Joe  34 83] to ['Joe','34','83']"{
+            $actual[3].name | Should BeExactly 'Joe'
+            $actual[3].age  | Should Be 34
+            $actual[3].cash | Should Be 83
+        }
     }
 }
