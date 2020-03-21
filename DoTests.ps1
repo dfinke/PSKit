a8c8529335f7b6f596b3d90da3501f7ecbbcc52d
dfinke <finked@hotmail.com>
2020-03-21 14:30:05 -0400

    remove commented code

--- DoTests.ps1
+++ DoTests.ps1
@@ -3,9 +3,7 @@ $PSVersionTable.PSVersion
 $psModules = 'Pester', 'PSStringScanner'
 
 foreach ($module in $psModules) {
-    #if ($null -eq (Get-Module -ListAvailable $module)) {
     Install-Module -Name $module -Repository PSGallery -Force -Scope CurrentUser
-    #}
 }
 
 $result = Invoke-Pester -Script $PSScriptRoot\__tests__ -Verbose -PassThru
@@ -13,9 +11,3 @@ $result = Invoke-Pester -Script $PSScriptRoot\__tests__ -Verbose -PassThru
 if ($result.FailedCount -gt 0) {
     throw "$($result.FailedCount) tests failed."
 }
-
-# "Build.RequestedForId $($env:Build.RequestedForId)" | Out-Host
-#ls env: | Out-Host
-# if ($env:BUILD_REQUESTEDFOR -like '*doug*') {
-#     "Time to make the donuts" | Out-Host
-# }
