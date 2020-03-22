e2f07630fefc7726eb6ea5287959eb8ab0f30a6d
dfinke <finked@hotmail.com>
2020-03-22 12:19:33 -0400

    added synopsis

--- CustomTypeData.ps1
+++ CustomTypeData.ps1
@@ -38,12 +38,20 @@ Update-TypeData -Force -TypeName Array -MemberType ScriptMethod -MemberName Scan
 }
 
 Update-TypeData -Force -TypeName Array -MemberType ScriptMethod -MemberName Head -Value {
+    <#
+        .Synopsis
+        This function returns the first n rows for the object based on position. It is useful for quickly testing if your object has the right type of data in it.
+    #>
     param($numberOfRows = 5)
 
     $this[0..($numberOfRows - 1)]
 }
 
 Update-TypeData -Force -TypeName Array -MemberType ScriptMethod -MemberName Tail -Value {
+    <#
+        .Synopsis
+        This function returns last n rows from the object based on position. It is useful for quickly verifying data, for example, after sorting or appending rows.
+    #>
     param($numberOfRows = 5)
 
     $this[  (-$numberOfRows)..-1]
